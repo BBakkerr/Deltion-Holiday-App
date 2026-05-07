61ace8affa320c03d049e6e627f26616c407f9c3
BBakkerr <beabakkerr@gmail.com>
2026-05-07 09:44:00 +0200

    I've imported the navigation for the headers.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {createStaticNavigation, useNavigation,} from 'react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-navigation/elements';
 export default function App() {
   return (
     <View style={styles.container}>
